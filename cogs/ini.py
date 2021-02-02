299abd11ccd08137c66bc8670a4d110b4c81edba
Joshua <68912574+HybridLeaksFN@users.noreply.github.com>
2021-02-02 22:56:41 +0000

    Update ini.py

--- cogs/ini.py
+++ cogs/ini.py
@@ -1,10 +1,14 @@
 import json
 import traceback
-
+import requests
 import aiofiles
 import aiohttp
 import discord
 from discord.ext import commands, tasks
+
+res = requests.get('https://api.nitestats.com/v1/epic/bearer')
+json = res.json()
+token = json['accessToken']
 
 
 class ini(commands.Cog):
@@ -20,37 +24,39 @@ class ini(commands.Cog):
             self.check.stop()
             self.check.start()
 
-    @tasks.loop(seconds=220)
+    @tasks.loop(seconds=150)
     async def check(self):
         await self.client.wait_until_ready()
         async with aiohttp.ClientSession() as cs:
+            parameter = {"Authorization": f"bearer 046856a662684c6991aacd64d73b168e"} #change token value to {token} if you dont want to update it every couple of hours
             async with cs.get(
-                    'https://api.peely.de/v1/ini') as data:
+                    'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/cloudstorage/system', headers=parameter) as data:
                 if data.status != 200:
                     return
                 new = await data.json()
         for i in new:
             async with aiohttp.ClientSession() as cs:
+                parameter = {"Authorization": f"bearer 046856a662684c6991aacd64d73b168e"} #change token value to {token} if you dont want to update it every couple of hours
                 async with cs.get(
-                        f'https://api.peely.de/v1/ini/files/{i}') as data:
+                        f'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/cloudstorage/system/{i["uniqueFilename"]}', headers=parameter) as data:
                     if data.status != 200:
                         continue
                     try:
-                        old = await (
-                            await aiofiles.open(f'Cache/ini/{i}', mode='r', encoding="utf8")).read()
+                        oldd = await (
+                            await aiofiles.open(f"Cache/ini/{i['filename']}", mode='r', encoding="utf8")).read()
                     except:
-                        old = ""
+                        oldd = ""
                     text = str(await data.text())
                     templist = []
                     changes = ""
-                    for oldline in old.splitlines():
+                    for oldline in oldd.splitlines():
                         if oldline not in text.splitlines():
                             if oldline == "":
                                 continue
                             changes += f"- {oldline}\n"
 
                     for line in text.splitlines():
-                        if line in old.splitlines():
+                        if line in oldd.splitlines():
                             continue
                         else:
                             changes += f"+ {line}\n"
@@ -59,15 +65,15 @@ class ini(commands.Cog):
                         if i2 == "":
                             continue
                         if len(i2) > 1500:
-                            async with aiofiles.open("Cache/temp.txt", mode="w+", encoding="utf8") as file:
+                            async with aiofiles.open("Cache/temp.txt", mode="w", encoding="utf8") as file:
                                 await file.write(str(i2))
                             file = discord.File("Cache/temp.txt")
-                            await self.client.get_channel(743191744161775758).send(
-                                f"Detected Changes in **{i}**", file=file)
+                            await self.client.get_channel(743191744161775758).send( #changed channel id to the leaker cord ini updates channel
+                                f"{role} Detected Changes in **{i['filename']}**", file=file)
                         else:
-                            await self.client.get_channel(743191744161775758).send(
-                                f"Detected changes in **{i}**\n```diff\n{i2}\n```")
-                    async with aiofiles.open(f"Cache/ini/{i}", mode="w+", encoding="utf8") as file:
+                            await self.client.get_channel(743191744161775758).send( #changed channel id to the leaker cord ini updates channel
+                                f"Detected changes in **{i['filename']}**\n```diff\n{i2}\n```")
+                    async with aiofiles.open(f"Cache/ini/{i['filename']}", mode="w", encoding="utf8") as file:
                         await file.write(text)
         print("Fertig")
 
